d98ac18ce4050ecc2205deeb989172d1dddd7d13
Sahil Kashyap <32007662+sahilkashyap64@users.noreply.github.com>
2025-09-10 01:38:12 +0530

    add not found page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import InvitePage from "./pages/InvitePage.jsx";
 import MatchPage from "./pages/MatchPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
-  { path: "/i/:token", element: <InvitePage /> },
-  { path: "/matches/:id", element: <MatchPage /> },
+  { path: "/i/:token/*", element: <InvitePage /> },
+  { path: "/matches/:id/*", element: <MatchPage /> },
+  { path: "*", element: <NotFoundPage /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
