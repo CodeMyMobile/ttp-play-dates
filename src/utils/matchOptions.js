4f79e6e9298212f6ab280f69ce8828edc1d0162f
Studio-18 <pmcochrane@gmail.com>
2025-10-07 08:40:31 -0700

    feat: add match edit experience

---
+++ src/utils/matchOptions.js
@@ -0,0 +1,43 @@
+export const DEFAULT_SKILL_LEVEL = "2.5 - Beginner";
+
+export const skillLevelOptions = [
+  {
+    value: "2.5 - Beginner",
+    label: "2.5",
+    desc: "Beginner",
+    color: "from-emerald-400 to-emerald-500",
+  },
+  {
+    value: "3.0 - Advanced Beginner",
+    label: "3.0",
+    desc: "Adv. Beginner",
+    color: "from-sky-400 to-sky-500",
+  },
+  {
+    value: "3.5 - Intermediate",
+    label: "3.5",
+    desc: "Intermediate",
+    color: "from-yellow-400 to-amber-500",
+  },
+  {
+    value: "4.0 - Adv. Intermediate",
+    label: "4.0",
+    desc: "Advanced",
+    color: "from-orange-400 to-orange-500",
+  },
+  {
+    value: "4.5+ - Advanced",
+    label: "4.5+",
+    desc: "Expert",
+    color: "from-red-400 to-red-500",
+  },
+];
+
+export const matchFormatOptions = [
+  "Singles",
+  "Doubles",
+  "Mixed Doubles",
+  "Dingles",
+  "Round Robin",
+  "Other",
+];
